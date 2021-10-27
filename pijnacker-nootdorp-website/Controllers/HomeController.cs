ed3636be09436067cb4296956c039a1f856fc2e6
Joseph Thomas <110737@st-maartenscollege.nl>
2021-10-27 12:44:09 +0200

    Verbind met database, toon alle database regels in tabel

--- pijnacker-nootdorp-website/Controllers/HomeController.cs
+++ pijnacker-nootdorp-website/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
 using pijnacker_nootdorp_website.Models;
 using System;
 using System.Collections.Generic;
@@ -20,7 +21,9 @@ namespace pijnacker_nootdorp_website.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<House> houses = GetHouses();
+
+            return View(houses);
         }
 
         public IActionResult Privacy()
@@ -32,6 +35,47 @@ namespace pijnacker_nootdorp_website.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public List<House> GetHouses()
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=110737;Uid=110737;Pwd=infsql2021;";
+            //string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110737;Uid=110737;Pwd=infsql2021;";
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<House> products = new List<House>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from houses", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        House house = new House
+                        {
+                            ID = Convert.ToInt32(reader["id"]),
+                            Address = reader["address"].ToString(),
+                            Price = Convert.ToInt32(reader["price"]),
+                            Description = reader["description"].ToString()
+                        };
+
+                        // voeg de naam toe aan de lijst met namen
+                        products.Add(house);
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return products;
         }
     }
 }
---
+++ pijnacker-nootdorp-website/Models/House.cs
@@ -0,0 +1,8 @@
+﻿public class House
+{
+    public int ID { get; set; }
+
+    public string Address { get; set; }
+    public int Price { get; set; }
+    public string Description { get; set; }
+}
