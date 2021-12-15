b860a0e3102ab0735696258f9efd51128bfb6ba8
Joseph Thomas <tinynerd001@gmail.com>
2021-12-15 15:49:20 +0100

    Maak structuur netjes naast specificaties

--- pijnacker-nootdorp-website/Scripts/TextUtilities.cs
+++ pijnacker-nootdorp-website/Scripts/TextUtilities.cs
@@ -13,4 +13,50 @@
     {
         return value == "on" ? true : false;
     }
+
+    public static string TranslateRoomType(string roomType, bool plural)
+    {
+        switch (roomType)
+        {
+            case "BEDROOM":
+                return plural ? "Slaapkamers" : "Slaapkamer";
+
+            case "LIVING_ROOM":
+                return plural ? "Woonkamers" : "Woonkamer";
+
+            case "KITCHEN":
+                return plural ? "Keukens" : "Keuken";
+
+            case "BATHROOM":
+                return plural ? "Badkamers" : "Badkamer";
+
+            case "TOILET":
+                return plural ? "Toilets" : "Toilets";
+
+            case "WORKSPACE":
+                return plural ? "Werkkamers" : "Werkkamer";
+        }
+
+        return roomType;
+    }
+
+    public static string TranslateStorey(int storey)
+    {
+        switch (storey)
+        {
+            case 0:
+                return "Begane grond";
+
+            case 1:
+                return "Eerste verdieping";
+
+            case 2:
+                return "Tweede verdieping";
+
+            case 3:
+                return "Derde verdieping";
+        }
+
+        return $"Verdieping {storey}";
+    }
 }
