15bfc03c286f2678e521a2a919e23c8954430bef
Joseph Thomas <tinynerd001@gmail.com>
2021-12-16 22:01:13 +0100

    Maak registratie pagina ook episch

--- pijnacker-nootdorp-website/Models/User.cs
+++ pijnacker-nootdorp-website/Models/User.cs
@@ -7,7 +7,7 @@ public class User
     [Required] public string FirstName { get; set; } = "";
     [Required] public string LastName { get; set; } = "";
 
-    [EmailAddress] public string Email { get; set; } = "";
+    [EmailAddress, Required] public string Email { get; set; } = "";
     [Required] public string Password { get; set; } = "";
 
     public virtual Order Order { get; set; }
